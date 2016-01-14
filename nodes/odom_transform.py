419454ccc73573018146b6d1d8cc5945f60a04d2
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2016-01-14 16:50:03 +0100

    Fixed odom_tranform py node

--- nodes/odom_transform.py
+++ nodes/odom_transform.py
@@ -3,47 +3,50 @@ import rospy
 import tf
 from std_srvs.srv import Empty, EmptyResponse
 
-restart = False
-playing = False
+class OdomTransform:
+    def __init__(self):
+        self.restart = False
+        self.playing = False
 
-def handle_beginning(req):
-    global restart
-    global playing
-    restart = True
-    playing = False
-    return EmptyResponse()
+    def handle_beginning(self, req):
+        self.restart = True
+        self.playing = False
+        return EmptyResponse()
+
+    def loop(self):
+        # Get params with defaults
+        rate = rospy.get_param('~rate', 100.0) # 100Hz
+        world_frame = rospy.get_param('~world_frame', '/world')
+        odom_frame = rospy.get_param('~odom_frame', '/odom')
+        base_link_frame = rospy.get_param('~base_link_frame', '/Robot_1/base_link')
+
+        # Publish service
+        rospy.Service('odom_transform_restart', Empty, self.handle_beginning)
+
+        listener = tf.TransformListener()
+
+        rate = rospy.Rate(rate)
+
+        while not rospy.is_shutdown():
+            while self.restart and not rospy.is_shutdown():
+                try:
+                    (trans,rot) = listener.lookupTransform(world_frame, base_link_frame, rospy.Time(0))
+                    self.restart = False
+                    self.playing = True
+                except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                    continue
+            if self.playing:
+                br = tf.TransformBroadcaster()
+                br.sendTransform(trans, rot,
+                                 rospy.Time.now(),
+                                 odom_frame,
+                                 world_frame)
+            rate.sleep()
 
 if __name__ == '__main__':
-    global restart
-    global playing
     # Init ROS node
     rospy.init_node('odom_transform')
 
-    # Get params with defaults
-    rate = rospy.get_param('~rate', 100.0) # 100Hz
-    world_frame = rospy.get_param('~world_frame', '/world')
-    odom_frame = rospy.get_param('~odom_frame', '/odom')
-    base_link_frame = rospy.get_param('~base_link_frame', '/Robot_1/base_link')
+    odom_transform_node = OdomTransform()
 
-    # Publish service
-    rospy.Service('odom_transform_restart', Empty, handle_beginning)
-
-    listener = tf.TransformListener()
-
-    rate = rospy.Rate(rate)
-
-    while not rospy.is_shutdown():
-        while restart and not rospy.is_shutdown():
-            try:
-                (trans,rot) = listener.lookupTransform(world_frame, base_link_frame, rospy.Time(0))
-                restart = False
-                playing = True
-            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                continue
-        if playing:
-            br = tf.TransformBroadcaster()
-            br.sendTransform(trans, rot,
-                             rospy.Time.now(),
-                             odom_frame,
-                             world_frame)
-        rate.sleep()
+    odom_transform_node.loop()
